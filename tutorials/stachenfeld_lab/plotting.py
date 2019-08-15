added documentation to notebook.ipynb in tutorials/stachenfeld_lab, fixed a numerical error that was coming up in rl.py:step, added cmap input to plotting.py:plot_many, deleted some nonsense files (plan, __pycache__)

tutorials/stachenfeld_lab/plotting.py
@@ -54,7 +54,7 @@ def plot_graph(adj, ax=None, xy=None, show_ticks=False,
 
 
 def plot_many_features(adj, xy, features, nrows=3, ncols=3, figsize=(12, 10), axes=None,
-                       node_size=300, norm_per_feature=False):
+                       node_size=300, norm_per_feature=False, cmap=None):
   """Plot first nrows*ncols features over graph."""
   if axes is None:
     fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=figsize)
@@ -65,17 +65,17 @@ def plot_many_features(adj, xy, features, nrows=3, ncols=3, figsize=(12, 10), ax
     vmax = np.max([features.max(), -features.min()])
     if features.min() < 0:
       vmin = -vmax
-      cmap = "RdBu"
+      cmap = "RdBu" if cmap is None else cmap
     else:
       vmin = features.min()
-      cmap = sns.cubehelix_palette(light=1, as_cmap=True)
+      cmap = sns.cubehelix_palette(light=1, as_cmap=True) if cmap is None else cmap
   else:
       vmin = None
       vmax = None
       if features.min() < 0:
-        cmap = "RdBu" 
+        cmap = "RdBu" if cmap is None else cmap
       else:
-        cmap = sns.cubehelix_palette(light=1, as_cmap=True)
+        cmap = sns.cubehelix_palette(light=1, as_cmap=True) if cmap is None else cmap
 
   k = 0
   for i in range(nrows):

tutorials/stachenfeld_lab/rl.py
@@ -7,9 +7,12 @@ def as_one_hot(ind, n):
     vec[ind] = 1
     return vec
 
-def step(state_ind, transmat):
+def step(state_ind, transmat, atol=1e-7):
     """Take a random step in the task specified by transition matrix."""
-    next_state = np.where(np.random.multinomial(1, transmat[state_ind].reshape(-1)))[0][0]
+    pvals = np.round(transmat[state_ind] / atol) * atol
+    pvals = pvals / np.sum(pvals)
+    next_state_onehot = np.random.multinomial(1, pvals=pvals)
+    next_state = np.where(next_state_onehot)[0][0]
     return next_state
 
 def value_update(reward, value, current_state, next_state, learning_rate, discount, features=None):